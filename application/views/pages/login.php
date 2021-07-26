36c363e8c96d0bab08e269c231601f48c8390d31
Paulo Ferreira <paulo.ferreira@sparkag.com.br>
2021-07-26 13:51:32 -0300

    Ajuste tela da login;

--- application/views/pages/login.php
+++ application/views/pages/login.php
@@ -3,56 +3,82 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<meta name="description" content="">
-	<meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
-	<meta name="generator" content="Jekyll v3.8.6">
 	<title><?= $titulo ?></title>
 
-	<link rel="canonical" href="https://getbootstrap.com/docs/4.4/examples/sign-in/">
-
 	<!-- Bootstrap core CSS -->
-	<link href="https://getbootstrap.com/docs/4.4/dist/css/bootstrap.min.css" rel="stylesheet"
-		  integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet"/>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+		  integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 	<meta name="theme-color" content="#563d7c">
 
 
 	<style>
-		.bd-placeholder-img {
-			font-size: 1.125rem;
-			text-anchor: middle;
-			-webkit-user-select: none;
-			-moz-user-select: none;
-			-ms-user-select: none;
-			user-select: none;
+		a {
+			text-decoration: none;
 		}
-
-		@media (min-width: 768px) {
-			.bd-placeholder-img-lg {
-				font-size: 3.5rem;
-			}
+		.login-page {
+			width: 100%;
+			height: 100vh;
+			display: inline-block;
+			display: flex;
+			align-items: center;
+		}
+		.form-right i {
+			font-size: 100px;
 		}
 	</style>
 	<!-- Custom styles for this template -->
-	<link href="https://getbootstrap.com/docs/4.4/examples/sign-in/signin.css" rel="stylesheet">
 </head>
 <body class="text-center">
-<form class="form-signin" method="post" action="<?php base_url() ?>login/logar">
-	<img class="mb-4" src="/application/assets/img/medico.png" alt="" width="72"
-		 height="72">
-	<h1 class="h3 mb-3 font-weight-normal">Login</h1>
+<div class="login-page bg-light">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-10 offset-lg-1">
+				<h3 class="mb-3">Clínica Médica</h3>
+				<div class="bg-white shadow rounded">
+					<div class="row">
+						<div class="col-md-7 pe-0">
+							<div class="form-left h-100 py-5 px-5">
+								<form action="<?php base_url() ?>login/logar" method="post" class="row g-4">
+									<div class="col-12">
+										<label for="usuario">Usuário<span class="text-danger">*</span></label>
+										<div class="input-group">
+											<div class="input-group-text"><i class="bi bi-person-fill"></i></div>
+											<input type="text" name="usuario" id="usuario" class="form-control"
+												   placeholder="Entre com o usuário" required autofocus>
+										</div>
+									</div>
 
-	<label for="usuario" class="sr-only">Usuário</label>
-	<input type="text" name="usuario" id="usuario" class="form-control" placeholder="Usuário" required
-		   autofocus>
+									<div class="col-12">
+										<label for="senha">Senha<span class="text-danger">*</span></label>
+										<div class="input-group">
+											<div class="input-group-text"><i class="bi bi-lock-fill"></i></div>
+											<input type="password" name="senha" id="senha" class="form-control" placeholder="Entre com a senha" required>
+										</div>
+									</div>
 
-	<label for="senha" class="sr-only">Senha</label>
-	<input type="password" name="senha" id="senha" class="form-control" placeholder="Senha" required>
+									<div class="col-sm-6">
+										<a href="<?php base_url() ?>cadastrar" class="float-end text-primary">Criar uma nova conta?</a>
+									</div>
 
-	<p>
-		<a href="<?php base_url() ?>cadastrar">Não possui um conta?</a>
-	</p>
-	<button class="btn btn-lg btn-primary btn-block" type="submit">Logar</button>
-	<p class="mt-5 mb-3 text-muted">&copy; Frameworks II</p>
-</form>
+									<div class="col-12">
+										<button type="submit" class="btn btn-primary px-4 float-end mt-4">Login</button>
+									</div>
+								</form>
+							</div>
+						</div>
+						<div class="col-md-5 ps-0 d-none d-md-block">
+							<div class="form-right h-100 bg-primary text-white text-center pt-5">
+								<i class="bi bi-person-square"></i>
+								<h2 class="fs-1">Bem Vindo!</h2>
+							</div>
+						</div>
+					</div>
+				</div>
+				<p class="text-end text-secondary mt-3">Frameworks II</p>
+			</div>
+		</div>
+	</div>
+</div>
 </body>
 </html>
